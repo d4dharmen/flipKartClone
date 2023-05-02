28eb4ac9b12c07998e73d42274633c609d5e4391
dharmendra <d4dharmen@gmail.com>
2023-05-02 08:32:48 -0700

    initial commit

---
+++ flipKartClone/FrontEnd/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1> hi from app</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa ratione quisquam, laboriosam quam modi voluptates odit dolores soluta illum vero.</p>
+    </>
+  );
+}
+
+export default App;
